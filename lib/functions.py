910eeef8acbc8acf7445cc4bb53a0d283e3341df
yassinyussuf <yassinyussuf414@gmail.com>
2024-05-21 11:12:45 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
+name="guido"
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print("Hello, " + name + "!")
     pass
 
 def greet_with_default(name="programmer"):
+    print("Hello, " + name +'!')
     pass
 
 def add(num1, num2):
+    num1=45
+    num2=55
+    return num1+num2
+
     pass
 
+add(45,55)
+
 def halve(number):
+    return number/2
     pass
